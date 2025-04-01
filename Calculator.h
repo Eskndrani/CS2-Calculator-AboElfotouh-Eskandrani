92ebd77f8df3957e9f989a70e4ed87fabd394d1e
Mahmoud Alaskndrani <ma7m0d_alx@aucegypt.edu>
2025-04-02 00:35:58 +0200

    Some member functions in Calculator class

---
+++ Calculator.h
@@ -0,0 +1,12 @@
+class Calculator {
+public:
+    double subtract(double, double);
+
+    double divide(double, double);
+
+    long long factorial(long long);
+
+    long long GCD(long long, long long);
+
+    long long Random(long long, long long);
+};
